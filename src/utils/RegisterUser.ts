decc290c6814ec2de11d7d78dcd8d7e391f8af32
Olanike Ogundiran <96661213+NikkyXO@users.noreply.github.com>
2024-09-03 22:47:32 +0100

    update register user

--- src/utils/RegisterUser.ts
+++ src/utils/RegisterUser.ts
@@ -1,4 +1,4 @@
-import { ZoKratesProvider, initialize, CompilationArtifacts, SetupKeypair, Proof } from "zokrates-js";
+import { ZoKratesProvider, initialize, CompilationArtifacts, SetupKeypair, Proof, VerificationKey } from "zokrates-js";
 import crypto from "crypto";
 const fs = require("fs");
 import * as dotenv from "dotenv";
@@ -23,15 +23,17 @@ class UserProof {
     }
 
     private async initializeZoKrates() {
+        const { initialize } = await import("zokrates-js");
         this.zokratesProvider = await initialize();
     }
 
     public async registerUser(data: UserData): Promise<string> {
         try {
-            const source: any = await fs.readFile("./eligibility.zok", "utf8");
+            const source: string = await fs.readFile("./eligibility.zok", "utf8");
 
             // Compilation
             const artifacts: CompilationArtifacts = this.zokratesProvider.compile(source);
+            console.log("Circuit compiled successfully.");
 
             // Computation
             const { year, month, day, voter_id, reducedHash } = data;
@@ -62,30 +64,57 @@ class UserProof {
         }
     }
 
-    private encryptProof(proof: any): string {
+    verifyProof(verificationKey: VerificationKey, proof: Proof): boolean | null {
+        try {
+          if (!this.zokratesProvider) {
+            throw new Error("ZoKrates provider is not initialized. Call initializeProvider() first.");
+          }
+    
+          const isVerified: boolean = this.zokratesProvider.verify(verificationKey, proof);
+          console.log("Proof verification result:", isVerified);
+          return isVerified;
+        } catch (err) {
+          console.error("Error verifying proof:", (err as Error).message);
+          return null;
+        }
+    }
+
+    private encryptProof(proof: Proof, keypair: SetupKeypair): string {
         const password = 'my secret key';
         const key = crypto.scryptSync(password, 'salt', 32);
         const iv = Buffer.alloc(16, 0);
-        const proofData = `${proof.a.join(",")}-${proof.b.map(b => b.join(",")).join("-")}-${proof.c.join(",")}`;
+        // const proofData = `${proof.a.join(",")}-${proof.b.map(b => b.join(",")).join("-")}-${proof.c.join(",")}`;
         const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-        let encrypted = cipher.update(proofData, 'utf8', 'hex');
+        let encrypted = cipher.update(JSON.stringify({ proof, keypair }), 'utf8', 'hex');
         encrypted += cipher.final('hex');
         return encrypted;
     }
 
-    private decryptProof(secretKey: string): Proof {
+    private decryptProof(secretKey: string): { proof: Proof, keypair: SetupKeypair } {
         const password = 'my secret key';
         const key = crypto.scryptSync(password, 'salt', 32);
         const iv = Buffer.alloc(16, 0);
         const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
         let decrypted = decipher.update(secretKey, 'hex', 'utf8');
         decrypted += decipher.final('utf8');
+        console.log(decrypted);
+        return JSON.parse(decrypted);
+        // return decrypted;
 
-        const [a, b, c] = decrypted.split("-");
-        return {
-            a: a.split(","),
-            b: [b.split(",").slice(0, 2), b.split(",").slice(2, 4)],
-            c: c.split(","),
-        };
     }
+
+    verifyProof2(decryptedData: any): boolean | null {
+        try {
+          if (!this.zokratesProvider) {
+            throw new Error("ZoKrates provider is not initialized. Call initializeProvider() first.");
+          }
+          const isVerified: boolean = this.zokratesProvider.verify(decryptedData.keypair.vk, decryptedData.proof);
+          console.log("Proof verification result:", isVerified);
+          return isVerified;
+        } catch (err) {
+          console.error("Error verifying proof:", (err as Error).message);
+          return null;
+        }
+    }
+
 }
